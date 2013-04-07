757969ed7c4b79ff9a475ec2fdbc23db5d4fe1ac
Shubhamoy <shubhamoy@kalakhatta.com>
2013-04-07 11:56:58 +0530

    Update vpsbackup.sh

--- vpsbackup.sh
+++ vpsbackup.sh
@@ -3,13 +3,13 @@
 # VPS Backup Script by Shubhamoy
 # Send your queries at shubhamoy[at]kalakhatta.com
 #=======================================================================================================================
-rm -rf backup.sql
-rm -rf backup.tar
+DATE=$(date +"%Y%m%d")
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
 PASS="Enter your MySQL Password"
-$MYSQLDUMP -u root -h localhost -p$PASS dbname > backup.sql
-$TAR -cvf backup.tar /var/www
-$TAR -rvf backup.tar backup.sql
-rm -rf backup.sql
-scp backup.tar user@remote-host:directory
+$MYSQLDUMP -u root -h localhost -p$PASS dbname > sqldump$DATE.sql
+$TAR -cvf $DATE.tar /var/www
+$TAR -rvf DATE.tar sqldump$DATE.sql
+scp $DATE.tar user@remote-host:directory
+rm -rf *.tar
+rm -rf *.sql
