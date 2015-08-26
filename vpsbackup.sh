bbe889a439eca924706b8984fd4789f4ffbd2b29
Shubhamoy <shubhamoy.chakrabarty@gmail.com>
2015-08-26 21:27:42 +0530

    Delete old backups excluding the new ones

--- vpsbackup.sh
+++ vpsbackup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #=======================================================================================================================
 # VPS Backup Script by Shubhamoy
+# This script will compress your webroot and add a database dump. After that it can transfer the file to another server
 # Send your queries at shubhamoy[at]kalakhatta.com
 #=======================================================================================================================
 DATE=$(date +"%Y%m%d")
@@ -8,9 +9,10 @@ MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
 PASS="Enter your MySQL Password"
 $MYSQLDUMP -u root -h localhost -p$PASS dbname > sqldump$DATE.sql
+# Configure the path for your web directory
+# Apache Default Web Root: /var/www
+# nGinx Default Web Root: /usr/share/nginx/html
 $TAR -zcvf $DATE.tar /var/www
 $TAR -rvf DATE.tar sqldump$DATE.sql
 scp $DATE.tar user@remote-host:directory
-#Issue: 1 Thanks to johnwbyrd. Basically this was added to remove the backup files. 
-#rm -rf *.tar
-#rm -rf *.sql
+rm -rf !($DATE.tar | sqldump$DATE.sql | vpsbackup.sh)
