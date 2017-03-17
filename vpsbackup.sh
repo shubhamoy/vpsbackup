704d7231e8024263cf52c0a5da7df3e40cb2e813
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-03-17 18:02:02 +0530

    Restructuring the code

--- vpsbackup.sh
+++ vpsbackup.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-#=======================================================================================================================
 # VPS Backup Script by Shubhamoy
-# This script will compress your webroot and add a database dump. After that it can transfer the file to another server
-# Send your queries at shubhamoy[at]kalakhatta.com
-#=======================================================================================================================
+# https://github.com/shubhamoy/vpsbackup
+
 DATE=$(date +"%Y%m%d")
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
 PASS="Enter your MySQL Password"
+
+# Similarly other db dumps can be generated like mongodump
 $MYSQLDUMP -u root -h localhost -p$PASS dbname > sqldump$DATE.sql
+
 # Configure the path for your web directory
-# Apache Default Web Root: /var/www
-# nGinx Default Web Root: /usr/share/nginx/html
+# Apache Default Web Root: /var/www or /home/www
+# nGinx Default Web Root: /usr/share/nginx/html or /home/www
+
+# Creating a tarball from the web directory
 $TAR -cvf $DATE.tar /var/www
+
+# Appending the db dump to the tarball
 $TAR -rvf $DATE.tar sqldump$DATE.sql
+
+# Move the backup to a different server
+# scp 20170317.tar ubuntu@54.xxx.xxx.xxx:/home/backup/
 scp $DATE.tar user@remote-host:directory
-rm -rf !"($DATE.tar | sqldump$DATE.sql | vpsbackup.sh)"
+
+# Delete the tarball and the db dump
+rm -rf !"($DATE.tar | sqldump$DATE.sql)"
