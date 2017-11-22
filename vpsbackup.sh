84e7c9e27ee4f6381c13a004bfd9d655db94b32f
fishnux <fishnux@riseup.net>
2017-11-22 23:30:27 +0000

    Add configuration section on vpsbackup.sh
    I've added a configuration section to the script, to make things a little bit prettier.

--- vpsbackup.sh
+++ vpsbackup.sh
@@ -2,27 +2,32 @@
 # VPS Backup Script by Shubhamoy
 # https://github.com/shubhamoy/vpsbackup
 
+# BEGIN configuration
+MYSQL_PASS="Enter your MySQL Password"
+BACKUP_DIRS="/var/www /home/me/stuff"
+SCP_TARGET="ubuntu@54.xxx.xxx.xxx:/home/backup/"
+# END configuration
+
 DATE=$(date +"%Y%m%d")
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
-PASS="Enter your MySQL Password"
 
 # Similarly other db dumps can be generated like mongodump
-$MYSQLDUMP -u root -h localhost -p$PASS dbname > sqldump$DATE.sql
+$MYSQLDUMP -u root -h localhost -p$MYSQL_PASS dbname > sqldump$DATE.sql
 
 # Configure the path for your web directory
 # Apache Default Web Root: /var/www or /home/www
 # nGinx Default Web Root: /usr/share/nginx/html or /home/www
 
 # Creating a tarball from the web directory
-$TAR -cvf $DATE.tar /var/www
+$TAR -cvf $DATE.tar $BACKUP_DIRS
 
 # Appending the db dump to the tarball
 $TAR -rvf $DATE.tar sqldump$DATE.sql
 
 # Move the backup to a different server
 # scp 20170317.tar ubuntu@54.xxx.xxx.xxx:/home/backup/
-scp $DATE.tar user@remote-host:directory
+scp $DATE.tar $SCP_TARGET
 
 # Delete the tarball and the db dump
 rm -rf !"($DATE.tar | sqldump$DATE.sql)"
